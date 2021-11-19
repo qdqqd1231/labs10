9b75b920ac7388a9d4e453eaf7dfb8809eb76448
Никита Макурин <makurin.nik@list.ru>
2021-11-19 22:51:52 +0300

    1

--- labs10/Oracle.cs
+++ labs10/Oracle.cs
@@ -8,6 +8,29 @@ namespace labs10
 {
     class Oracle : Point
     {
+        public double radius { get; private set; }
+
+
+        public double square { get; private set; }
+
+        public Oracle(double xCenter, double yCenter, ConsoleColor color, double radius) : base(xCenter, yCenter, color)
+        {
+            this.radius = radius;
+            square = Math.PI * radius * radius;
+        }
+        public Oracle(double xCenter, double yCenter, double radius) : base(xCenter, yCenter)
+        {
+            this.radius = radius;
+            square = Math.PI * radius * radius;
+        }
+        public  void PrintInfo()
+        {
+            Console.WriteLine($"Цвет фигуры {color}");
+            Console.WriteLine($"Абсцисса точки центра окружности {x}, ордината точки окружности {y}");
+            Console.WriteLine($"Видимость фигуры {visibleStatus}");
+            Console.WriteLine($"Радиус окружности {radius}");
+            Console.WriteLine($"Площадь круга {square}");
+        }
 
     }
 }
--- labs10/Rectangle.cs
+++ labs10/Rectangle.cs
@@ -8,6 +8,37 @@ namespace labs10
 {
     class Rectangle : Point
     {
+        public double y2 { get; private set; }
+        public double x2 { get; private set; }
+        public double height { get; private set; }
+        public double width { get; private set; }
+        public double square { get; private set; }
+
+        public Rectangle(double x1, double y1, double x2, double y2, ConsoleColor color) : base(x1, y1, color)
+        {
+            this.y2 = y2;
+            this.x2 = x2;
+            width = Math.Abs(x2 - x1);
+            height = Math.Abs(y2 - y1);
+            square = width * height;
+        }
+        public Rectangle(double x1, double y1, double x2, double y2) : base(x1, y1)
+        {
+            this.y2 = y2;
+            this.x2 = x2;
+            width = Math.Abs(x2 - x1);
+            height = Math.Abs(y2 - y1);
+            square = width * height;
+        }
+      
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Цвет фигуры {color}");
+            Console.WriteLine($"Две точки, по которым задается прямоугольник ({x},{y}); ({x2},{y2})");
+            Console.WriteLine($"Высота прямоугольника {height}, длина {width}");
+            Console.WriteLine($"Видимость фигуры {visibleStatus}");
+            Console.WriteLine($"Площадь прямоугольника {square}");
+        }
 
     }
 }
